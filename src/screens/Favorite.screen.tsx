2add5183995d966bc4ee4d5f33e47f23a50bb320
Emeka Uja <ujadninth@gmail.com>
2022-01-28 22:53:43 +0100

    added screens ui

--- src/screens/Favorite.screen.tsx
+++ src/screens/Favorite.screen.tsx
@@ -1,23 +1,58 @@
 import React from 'react';
-import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import { useAppContext } from '../App.provider';
+import { Container } from '../components/Container';
+import { LikedCatCard } from '../components/LikedCatCard';
 
 export const Favorite: React.FC = () => {
   const appContext = useAppContext();
+
   return (
-    <ScrollView>
-      <Text>Favorite</Text>
-      <Text>
-        {appContext.likedCatList.map(item => (
-          <Text>{item.cat.name}</Text>
-        ))}
-      </Text>
-    </ScrollView>
+    <Container>
+      {appContext.likedCatList.length ? (
+        <View style={styles.listContainer}>
+          {appContext.likedCatList.map((likedCat, index) => (
+            <View style={styles.item}>
+              <LikedCatCard
+                key={`${likedCat.cat.id + index}`}
+                imageUrl={likedCat.cat.imageUrl}
+                name={likedCat.cat.name}
+                removeFromFave={() => appContext.handleDeleteCat(likedCat)}
+                liked={likedCat.liked}
+              />
+            </View>
+          ))}
+        </View>
+      ) : (
+        <View style={styles.text}>
+          <Text>Like a cat to see your fave lineup!</Text>
+        </View>
+      )}
+    </Container>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexGrow: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 25,
+    paddingTop: 25,
+  },
+  text: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start', // if you want to fill rows left to right
+  },
+  item: {
+    maxWidth: Dimensions.get('window').width / 2, // is 50% of container width: ;
+    flex: 0.5,
+    marginBottom: 20,
   },
 });
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,12 +1,65 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, FlatList, View, Text, Dimensions } from 'react-native';
+import { useAppContext } from '../App.provider';
 import { CatCard } from '../components/CatCard';
+import { ItemListLoader } from '../components/ItemList';
+import AppApi from '../gateway/api';
+import { IBreed } from '../types';
 
-export const Home: React.FC = () => {
+export const Home: React.FC = ({}) => {
+  const appContext = useAppContext();
+
+  const [catsList, setCatsList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchBreeds();
+  }, []);
+  const fetchBreeds = () => {
+    AppApi.get('v1/breeds')
+      .then(async response => {
+        setLoading(false);
+        setCatsList(response.data);
+        console.log(response);
+      })
+      .catch(error => console.log(error));
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.loaderContainer}>
+        <ItemListLoader backgroundColor="#efefee" highlightColor="#fff" />
+      </View>
+    );
+  }
+
+  if (!catsList) {
+    return (
+      <View style={styles.container}>
+        <Text>Sorry! No cats here</Text>
+      </View>
+    );
+  }
+  const renderCatItem = ({ item }: { item: IBreed }) => {
+    return (
+      <CatCard
+        id={item.id}
+        imageUrl={item?.image?.url}
+        name={item?.name}
+        addToFave={catItem => appContext.handleCatLike(catItem)}
+        removeFromFave={catItem => appContext.handleDeleteCat(catItem)}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <CatCard />
+      <FlatList
+        keyExtractor={item => item.id.toString()}
+        data={catsList}
+        showsVerticalScrollIndicator={false}
+        renderItem={renderCatItem}
+      />
     </View>
   );
 };
@@ -14,6 +67,15 @@ export const Home: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexGrow: 1,
     backgroundColor: '#fff',
+    paddingHorizontal: Dimensions.get('window').width / 20,
+    paddingTop: 20,
+  },
+  loaderContainer: {
+    flex: 1,
+    flexGrow: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 25,
   },
 });
